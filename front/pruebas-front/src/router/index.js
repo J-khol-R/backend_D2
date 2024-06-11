e6ad7a2740c25605d0cf3a3e59105a92d9016b2e
Kevin Ordoñez <kevin.2000villegas@gmail.com>
2024-06-10 21:28:25 -0500

    feat: Add UsersView component and route

--- front/pruebas-front/src/router/index.js
+++ front/pruebas-front/src/router/index.js
@@ -4,6 +4,7 @@ import UserRegister from '../views/UserRegister.vue';
 import HomeView from '../views/HomeView.vue';
 import CreateProject from '../views/CreateProject.vue';
 import HomeProjects from '../views/HomeProjects.vue';
+import UsersView from '../views/UsersView.vue';
 
 const routes = [
   {
@@ -29,6 +30,11 @@ const routes = [
     path: '/create-project',
     name: 'CreateProject',
     component: CreateProject,
+  },
+  {
+    path: '/users',
+    name: 'Users',
+    component: UsersView,
   },
   {
     path: '/:pathMatch(.*)*',
